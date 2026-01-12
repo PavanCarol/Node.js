9124a0fa127e4f2a8b426f11a8a0fe954ea62770
PavanCarol <pavanm.carolina@gmail.com>
2026-01-12 08:48:58 -0300

    --

--- 01-fundamentos-node/src/server.js
+++ 01-fundamentos-node/src/server.js
@@ -8,17 +8,32 @@
 //com o "type": "module",
 import http from 'node:http'
 
+//Cabeçalhos(Requisição/resposta) => Metadados
+
+//Stateful => (dados armazenados localmente em memoria) - Stateless
+const users = []
 
 
 const server = http.createServer((req, res) => {
+    //rodar aplicação : npm run dev
     const { method, url } = req
-
+    
     if(etho === 'GET' && url === '/users'){
-        return res.end('Listem do usuários')
+        //Early return
+        //JSON -JS Object Notation
+        return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
     }
 
     if(method === 'POST' && url === '/users'){
-        return res.end('Criação de usuário')
+
+        users.push({
+            id:1,
+            name:'John Doe',
+            email:'jodoe@example.com',
+        })
+        return res.writeHead(201).end()
     }
     
     console.log(method,url)
