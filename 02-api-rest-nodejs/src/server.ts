2d7963f33fe3991e94cb5e1672b79835adb75125
PavanCarol <pavanm.carolina@gmail.com>
2026-01-12 11:16:01 -0300

    --Banco de dados

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,17 +1,29 @@
 import fastify = require("fastify")
+import crypto from 'node:crypto'
 import { knex } from "./database"
+import { env } from "./env"
 
 const app = fastify()
 
 //GET,POST,PUT,PATCH,DELTE
 
 app.get('/hello', async () => {
-    const tables = await knex('sqlite_schema').select('*')
-    return tables
+    // const transaction = await knex('transactions').insert({
+    //     id: crypto.randomUUID(),
+    //     title:'Transação de teste',
+    //     amount:1000
+    // }).returning('*')
+
+    const transaction = await knex('transactions')
+    .where('amount',1000)
+    .select('*')
+
+
+    return transaction
 })
 
 app.listen({
-    port:3333, 
+    port: env.PORT, 
 }).then(() => {
     console.group('HTTP')
 })
